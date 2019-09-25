88f0c2facbba6bc384622f460de4edd2eba19e32
Ruheni Alex <aleruheni@outlook.com>
2019-09-26 01:39:42 +0300

    project init

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/user/:name', function(req, res, next) {
+	res.render('index', { title: req.params.name });
+});
+
+module.exports = router;
